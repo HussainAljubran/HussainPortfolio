7358b457f360d3411615cf8fe68afdbcc37a8134
Hussain Aljubran <162879335+HussainAljubran@users.noreply.github.com>
2024-03-21 08:37:29 +0000

    build contact us and details buttons

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import load_projects_from_db, load_project_from_db, add_message_to_db
 
 app = Flask(__name__)
-projects = [{
-    "id": 1,
-    "project": "Web Scraping",
-    "skills": "urllib python, Python Requests, Selenium, BeautifulSoup"
-}, {
-    "id":
-    2,
-    "project":
-    "AI model for flare mointoring",
-    "skills":
-    "Python pandas, sikitlib, matblotlib, microsoft azure, pychatgpt"
-}]
 
 
 @app.route("/")
 def hello_world():
+  projects = load_projects_from_db()
   return render_template("home.html", projects=projects)
+
+
+@app.route("/project/<id>")
+def project_details(id):
+  project = load_project_from_db(id)
+  return render_template("project_details.html", project=project)
+
+@app.route("/message")
+def message():
+  return render_template("contactus.html")
+
+@app.route("/message_submitted", methods=["POST"])
+def message_submitted():
+  mdata = request.form
+  add_message_to_db(mdata)
+  return render_template("message_submitted.html", data=mdata)
+
 
 
 if __name__ == "__main__":
